dc4b7f5d6992b244ef5ad266a87ac8df3f09ad19
Luis Mendes <machadoluismendes@gmail.com>
2024-03-14 19:37:28 -0300

    Increment
    npm install --save-dev typescript ts-node nodemon

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+// ChatGPT 
+// src/index.ts
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+const port = 3000;
+
+// Middleware to log requests
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
