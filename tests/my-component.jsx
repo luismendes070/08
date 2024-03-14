7b46e7ae1729edce402e9eca211ed0bb3623e419
Luis Mendes <machadoluismendes@gmail.com>
2024-03-14 16:47:21 -0300

    Initial commit

--- tests/my-component.jsx
+++
@@ -1,4 +0,0 @@
-// https://testing-library.com/docs/react-testing-library/setup
-
-- import { render, fireEvent } from '@testing-library/react';
-+ import { render, fireEvent } from '../test-utils';
