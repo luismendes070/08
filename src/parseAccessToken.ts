21b6ee2c029a0c7ea54fc4b35b7bb0ed2c7b9c6a
Luis Mendes <machadoluismendes@gmail.com>
2024-03-15 08:38:39 -0300

    Increment
    api parses access token
    .env ISSUER_BASE_URL=${ISSUER_BASE_URL}

---
+++ src/parseAccessToken.ts
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const { auth } = require('express-oauth2-jwt-bearer');
+
+const port = process.env.PORT || 8080;
+
+const jwtCheck = auth({
+  audience: '${AUDIENCE}',
+  issuerBaseURL: '${ISSUER_BASE_URL}',
+  tokenSigningAlg: 'RS256'
+});
+
+// enforce on all endpoints
+app.use(jwtCheck);
+
+app.get('/authorized', function (req, res) {
+    res.send('Secured Resource');
+});
+
+app.listen(port);
+
+console.log('Running on port ', port);
